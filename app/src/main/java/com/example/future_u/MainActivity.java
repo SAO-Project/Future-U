b23667380372d407749f87db0406d127999d8b63
Esteban Retana <netherblade619@gmail.com>
2019-11-23 15:52:17 -0700

    added PDFReader

--- app/src/main/java/com/example/future_u/MainActivity.java
+++ app/src/main/java/com/example/future_u/MainActivity.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.tom_roush.pdfbox.util.PDFBoxResourceLoader;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        setup();
+    }
+
+    public void setup(){
+        PDFBoxResourceLoader.init(getApplicationContext());
     }
 }
---
+++ app/src/main/java/com/example/future_u/assessment/resume/PDFReader.java
@@ -0,0 +1,50 @@
+package com.example.future_u.assessment.resume;
+
+import android.content.res.AssetManager;
+import android.util.Log;
+
+import com.tom_roush.pdfbox.pdmodel.PDDocument;
+import com.tom_roush.pdfbox.text.PDFTextStripper;
+
+import java.io.IOException;
+/*
+* create instance to read pdf
+*   - use PDFReader.read(pathName) to read pdf
+ */
+public class PDFReader {
+    AssetManager assetManager;
+    public String read(String pathName){
+        String parsedText = null;
+        PDDocument document = null;
+        try {
+//            loads document
+            document = PDDocument.load(assetManager.open(pathName));
+        } catch(IOException e) {
+            Log.e("PdfBox-Android-Sample", "Exception thrown while loading document to strip", e);
+        }
+
+//        reads text
+        try {
+            PDFTextStripper pdfStripper = new PDFTextStripper();
+            pdfStripper.setStartPage(0);
+            pdfStripper.setEndPage(1);
+            parsedText = pdfStripper.getText(document);
+
+            return parsedText;
+        }
+        catch (IOException e)
+        {
+            Log.e("PdfBox-Android-Sample", "Exception thrown while stripping text", e);
+        } finally {
+            try {
+                if (document != null) document.close();
+            }
+            catch (IOException e)
+            {
+                Log.e("PdfBox-Android-Sample", "Exception thrown while closing document", e);
+            }
+        }
+        return null;
+
+    }
+}
