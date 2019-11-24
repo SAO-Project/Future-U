112927817bbe30f58a684b440e5081b90f2d21f3
Eddie Garcia <edd1e234g@gmail.com>
2019-11-23 17:07:43 -0700

    Created Skeleton of Assessment

---
+++ app/src/main/java/com/example/future_u/assessment/Assessment.java
@@ -0,0 +1,12 @@
+package com.example.future_u.assessment;
+
+public class Assessment {
+    private String path, major;
+    private Double gpa;
+
+    public Assessment(String path, String major, Double gpa) {
+        this.path = path;
+        this.major = major;
+        this.gpa = gpa;
+    }
+}
